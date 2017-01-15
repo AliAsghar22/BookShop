58c3340c32b04d0eb95e936896c7e5b589085bab
Ali Asghar <taghizade12@gmail.com>
2017-01-15 10:25:20 +0330

    some tiny change

--- Book/models.py
+++ Book/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+
 class Publisher(models.Model):
     name = models.CharField(max_length=30)
     address = models.CharField(max_length=50)
@@ -7,14 +9,17 @@ class Publisher(models.Model):
     website = models.URLField()
 
     def __str__(self):
-        return '%s , %s , %s' %(self.name, self.address , self.city )
+        return '%s , %s , %s' % (self.name, self.address, self.city)
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=40)
     email = models.EmailField()
+
     def __str__(self):
-        return '%s , %s , %s' %(self.first_name, self.last_name , self.email )
+        return '%s , %s , %s' % (self.first_name, self.last_name, self.email)
+
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
