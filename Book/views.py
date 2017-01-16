a60b2d3dc34fb2d0fbfc41ac56e75bd7b9115925
Ali Asghar <taghizade12@gmail.com>
2017-01-16 08:35:14 +0330

    hi

--- Book/views.py
+++ Book/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse ,HttpResponseRedirect
+from django.template import RequestContext ,loader
+
+def home(request):
+    page = loader.get_template('index.html')
+    return HttpResponse(page.render())
