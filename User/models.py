0f8553318a717213c38ce5cd22ee4be60f768658
Ali Asghar <taghizade12@gmail.com>
2017-01-16 08:51:07 +0330

    deleted unnececery file

--- User/models.py
+++ User/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model, User):
+    token = models.CharField(max_length=48)
